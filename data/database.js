33e895d5631ef49768efdf9be6028228d735571f
rami <lol@lolmail.com>
2023-11-05 00:24:44 -0400

    initial commit

--- data/database.js
+++ data/database.js
@@ -4,6 +4,7 @@ const MongoClient = mongodb.MongoClient;
 
 let database;
 let mongodbUrl = "mongodb://127.0.0.1:27017";
+console.log(process.env.MONGODB_URL);
 if (process.env.MONGODB_URL) {
   mongodbUrl = process.env.MONGODB_URL;
 }
