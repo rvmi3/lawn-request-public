bfee2a93bcc0820a3f60cb67c31003238a0817a7
rami <lol@lolmail.com>
2023-11-04 20:03:39 -0400

    initial commit

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ const MongoClient = mongodb.MongoClient;
 let database;
 let mongodbUrl = "mongodb://127.0.0.1:27017";
 if (process.env.MONGODB_URL) {
-  mongodb = process.env.MONGODB_URL;
+  mongodbUrl = process.env.MONGODB_URL;
 }
 async function connectToDatabase() {
   const client = await MongoClient.connect(mongodbUrl);
