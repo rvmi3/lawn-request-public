11a5e8de10b873d75061e4267cb21cae6735d4c7
rami <lol@lolmail.com>
2023-11-11 14:51:57 -0500

    Update commit

--- public/scripts/searchSuggestions.js
+++ public/scripts/searchSuggestions.js
@@ -2,13 +2,11 @@ const pages = document.getElementsByClassName("page-link");
 
 const urlParams = new URLSearchParams(window.location.search);
 const search = urlParams.get("search");
+const unavailable = urlParams.get("unavailable");
+console.log(unavailable);
 console.log(search);
 for (const page of pages) {
   page.addEventListener("click", async function (event) {
-    if (!search) {
-      window.location.href = `/?page=${event.target.innerText}`;
-      return;
-    }
-    window.location.href = `/?page=${event.target.innerText}&search=${search}`;
+    window.location.href = `/?page=${event.target.innerText}&search=${search}&unavailable=${unavailable}`;
   });
 }
